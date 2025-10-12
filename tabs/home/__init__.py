4711e05a8614289b3d7bc642f532b442e460d4b4
cem5113 <cem5113@hotmail.com>
2025-10-13 00:37:56 +0300

    Create __init__.py

---
+++ tabs/home/__init__.py
@@ -0,0 +1,10 @@
+# tabs/home/__init__.py
+from .view import render
+
+def register():
+    return {
+        "key": "home",
+        "title": "Ana Sayfa",
+        "icon": "🏠",
+        "render": render,
+    }
