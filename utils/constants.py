9f4cb9cee96fb9967fb208f67c650ace0429388c
cem5113 <cem5113@hotmail.com>
2025-10-14 01:16:51 +0300

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,10 @@
 # crimepredict/utils/constants.py
-from crimepredict.utils.constants import *
+
+# GEOID gibi temel anahtar isimleri burada tutulur.
+KEY_COL = "geoid"
+
+# San Francisco saat farkı (UTC -8)
+SF_TZ_OFFSET = -8
+
+# Varsayılan harita merkezi (lat, lon)
+SF_CENTER = (37.7749, -122.4194)
