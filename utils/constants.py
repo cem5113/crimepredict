3580459cb039a2d77a28f22a937429fbbf3a32fd
cem5113 <cem5113@hotmail.com>
2025-10-14 23:05:31 +0300

    Delete utils directory

--- utils/constants.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-KEY_COL = "GEOID"        # tüm merge ve groupby anahtarı
-RISK_COL = "risk_score"  # kayan noktalı risk skor kolon adı (float)
-TIME_COL = "timestamp"   # varsa tarih/saat (UTC/yerel fark etmiyor)
-DATE_COL = "date"        # günlük özet için kullanılabilir
