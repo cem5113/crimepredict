ae3b287b1e08e7859de931a0752ee9f1cfba6faa
cem5113 <cem5113@hotmail.com>
2025-10-25 16:27:15 +0300

    Update 4_🚓_Devriye_Planlama.py

--- pages/4_🚓_Devriye_Planlama.py
+++ pages/4_🚓_Devriye_Planlama.py
@@ -2,7 +2,7 @@ import streamlit as st
 from ui.tab_planning import render_planning  # ui/tab_planning.py
 
 from components.last_update import show_last_update_badge
-from components.utils import MODEL_VERSION, MODEL_LAST_TRAIN
+from components.meta import MODEL_VERSION, MODEL_LAST_TRAIN
 
 st.set_page_config(page_title="🚓 Devriye Planlama", layout="wide")
 render_planning()
