a9b656e15b9aa57b72e3c9551851e4184d1d99be
cem5113 <cem5113@hotmail.com>
2025-10-25 15:20:50 +0300

    Update utils.py

--- components/utils.py
+++ components/utils.py
@@ -1,4 +1,6 @@
+# components/utils.py
 from datetime import datetime
 
+# İsterseniz burayı dinamik hale getirip bir metadata JSON'dan da çekebilirsiniz.
 MODEL_VERSION = "v1.0.0"
-MODEL_LAST_TRAIN = datetime(2025, 10, 25, 0, 0, 0).strftime("%Y-%m-%d %H:%M UTC")
+MODEL_LAST_TRAIN = datetime(2025, 1, 1, 0, 0).strftime("%Y-%m-%d %H:%M UTC")
