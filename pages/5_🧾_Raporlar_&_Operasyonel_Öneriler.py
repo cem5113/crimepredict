be1a1d35eff39c4c2fa11a92b740116565ae0172
cem5113 <cem5113@hotmail.com>
2025-10-25 15:21:55 +0300

    Update 5_🧾_Raporlar_&_Operasyonel_Öneriler.py

--- pages/5_🧾_Raporlar_&_Operasyonel_Öneriler.py
+++ pages/5_🧾_Raporlar_&_Operasyonel_Öneriler.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from ui.tab_reports import render_reports  # ui/tab_reports.py
 
+from components.last_update import show_last_update_badge
+from components.utils import MODEL_VERSION, MODEL_LAST_TRAIN
+
 st.set_page_config(page_title="🧾 Raporlar & Operasyonel Öneriler", layout="wide")
 render_reports()
