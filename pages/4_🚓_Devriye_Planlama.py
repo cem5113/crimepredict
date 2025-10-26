2bb6f658840150d8e74ffcfdc5abc88f9aa66393
cem5113 <cem5113@hotmail.com>
2025-10-26 17:10:02 +0300

    Update 4_🚓_Devriye_Planlama.py

--- pages/4_🚓_Devriye_Planlama.py
+++ pages/4_🚓_Devriye_Planlama.py
@@ -5,4 +5,3 @@ from components.last_update import show_last_update_badge
 from components.meta import MODEL_VERSION, MODEL_LAST_TRAIN
 
 st.set_page_config(page_title="🚓 Devriye Planlama", layout="wide")
-render_planning()
